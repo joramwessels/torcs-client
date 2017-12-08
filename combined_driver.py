9a76d17b09ad877863f7d7f05c577dbf7b4712c0
joramwessels <joram.wessels@outlook.com>
2017-12-08 16:46:14 +0100

    Crisis handling framework implementation done. Untested. Kept the old implementation as an approach.

--- combined_driver.py
+++ combined_driver.py
@@ -15,7 +15,7 @@ from operator import sub
 
 from basic_control import BasicControl
 from swarm import FeromoneTrail
-from crisis_driver import CrisisDriver, car_is_stuck
+from crisis_driver import CrisisDriver
 
 # - NOTE crash detection for swarm only checks for off road
 # - NOTE collision detection for swarm might be too sensitive
@@ -52,7 +52,7 @@ class Final_Driver(Driver):
         super(Final_Driver, self).__init__()
         self.iter = 0
         self.basic_control = BasicControl(steering_values)
-        self.back_up_driver = Driver(logdata=False)
+        self.back_up_driver = CrisisDriver(logdata=False)
         self.bad_counter = 0
         self.lap_counter = 0
         self.last_opponents = [0 for x in range(36)]
@@ -79,6 +79,7 @@ class Final_Driver(Driver):
             command:    The next move packed in a Command object
 
         """
+        self.back_up_driver.update_status(carstate)
 
         # trackers
         self.update_trackers(carstate)
@@ -86,41 +87,25 @@ class Final_Driver(Driver):
             self.print_trackers(carstate, r=True)
 
         # crash and collision detection for swarm
-        if is_offroad(carstate):
-            self.crashed_in_last_frame = True
-        for dist in carstate.opponents:
-            if dist == 0:
-                self.contact_in_last_frame = True
+        if ENABLE_SWARM:
+            if self.back_up_driver.is_off_road:
+                self.crashed_in_last_frame = True
+            for dist in carstate.opponents:
+                if dist == 0:
+                    self.contact_in_last_frame = True
 
         # crisis handling
         if ENABLE_CRISIS_DRIVER:
             if self.back_up_driver.is_in_control:
                 return self.back_up_driver.drive(carstate)
-            elif car_is_stuck():
+            elif self.back_up_driver.needs_help:
                 self.back_up_driver.pass_control(carstate)
                 return self.back_up_driver.drive(carstate)
-
-        # bad place detection
-        if in_a_bad_place(carstate, self.bad_counter):
-            command = self.back_up_driver.drive(carstate)
-            if self.bad_counter >= 600 and is_stuck(carstate):
-                # we try reversing
-                command.gear = -command.gear
-                if command.gear < 0:
-                    command.steering = -command.steering
-                    command.gear = -1
-                self.bad_counter = 200
-        else:
-            # since the data and python's values differ we need to adjust them
-            carstate.angle = radians(carstate.angle)
-            carstate.speed_x = carstate.speed_x*3.6
-            command = self.make_next_command(carstate)
-            # based on target, implement speed/steering manually
-        if PRINT_COMMAND:
-            print("Executing command: gear={}, acc={}, break={}, steering={}".format(command.gear,
-                                                                                 command.accelerator,
-                                                                                 command.brake,
-                                                                                 command.steering))
+        
+        # since the data and python's values differ we need to adjust them
+        carstate.angle   = radians(carstate.angle)
+        carstate.speed_x = carstate.speed_x*3.6
+        command = self.make_next_command(carstate)
 
         return command
 
@@ -134,12 +119,7 @@ class Final_Driver(Driver):
             command:    The command object to pass back to the server
 
         """
-        command = Command()
 
-        # we switch gears manually
-        gear = self.basic_control.gear_decider(carstate)
-        # we get the steering prediction
-        steer_pred = self.basic_control.steer_decider(carstate)
         # checking in on the swarm
         position = carstate.distance_from_start
         new_frame = position > (self.previous_frame_position + self.swarm.pos_int)
@@ -158,9 +138,12 @@ class Final_Driver(Driver):
             self.contact_in_last_frame = False
             self.previous_frame_position = position
 
-        pedal = self.basic_control.speed_decider(carstate, max_speed=self.max_speed)
+        # basic predictions
+        gear       = self.basic_control.gear_decider(carstate)
+        steer_pred = self.basic_control.steer_decider(carstate)
+        pedal      = self.basic_control.speed_decider(carstate, max_speed=self.max_speed)
 
-        # make sure we don't drive at people
+        # making sure we don't drive at people
         opponents_deltas = list(map(sub, carstate.opponents, self.last_opponents))
         steer_pred, pedal = self.basic_control.deal_with_opponents(steer_pred,
                                                               pedal,
@@ -168,13 +151,22 @@ class Final_Driver(Driver):
                                                               carstate.distance_from_center,
                                                               carstate.opponents,
                                                               opponents_deltas)
-        # disambiguate pedal with smoothing
-        brake, accel = self.basic_control.disambiguate_pedal(pedal, accel_cap=1.0)
 
+        # disambiguating pedal with smoothing
+        brake, accel = self.basic_control.disambiguate_pedal(pedal, accel_cap=1.0)
+        
+        # debug output
+        if PRINT_COMMAND and self.iter % PRINT_CYCLE_INTERVAL:
+            print("Executing comand: gear=%.2f, acc=%.2f," %(gear, accel),
+                  "break=%.2f, steering=%.2f" %(brake, steer_pred))
+
+        # command construction
+        command = Command()
         command.brake = brake
         command.accelerator = accel
         command.steering = steer_pred
         command.gear = gear
+
         return command
 
     def update_trackers(self, carstate):
@@ -191,48 +183,6 @@ class Final_Driver(Driver):
                  carstate.distance_raced,
                  self.cummulative_time + carstate.current_lap_time)
                , end=line_end)
-
-def in_a_bad_place(carstate, bad_counter):
-    """ Description
-
-    Args:
-        carstate:       The full carstate object
-        bad_counter:    ???
-
-    Returns:
-        A boolean indicating whether the car is having troubles
-
-    """
-    something_wrong = False
-    if is_offroad(carstate):
-        #print("I'm offroad!")
-        something_wrong = True
-    if is_reversed(carstate):
-        #print("I'm reversed!")
-        something_wrong = True
-    if is_stuck(carstate):
-        #print("I'm stuck!")
-        something_wrong = True
-    if (something_wrong):
-        bad_counter += 1
-    else:
-        bad_counter = 0
-    # if we have been in a bad place for 2 seconds
-    if bad_counter >= 100:
-        return True
-    return False
-
-def is_offroad(carstate):
-    """ Returns True if car is off road """
-    return max(carstate.distances_from_edge) == -1
-
-def is_stuck(carstate):
-    """ Returns True if car is stuck """
-    return abs(carstate.speed_x) <= 5 and carstate.current_lap_time >= 10
-
-def is_reversed(carstate):
-    """ Returns True if the car is facing the wrong side """
-    return abs(carstate.angle) >= 90
 
 def err(*args):
     """ prints to standard error """
--- crisis_driver.py
+++ crisis_driver.py
@@ -1,7 +1,7 @@
 # file:         crisis_driver.py
 # author:       Joram Wessels
 # date:         07-12-2017
-# dependencies: -
+# dependencies: numpy, pytocl
 # description
 # usage
 #
@@ -9,12 +9,26 @@
 from pytocl.driver import Driver
 from pytocl.car import State, Command
 
-# margins of error when trying the reverse() approach
-REV_CTR_MARGIN = 1
-REV_ANG_MARGIN = 10
-REV_ACC = 0.4
-CAREFUL_REV_ACC = 0.2
+from sys import stderr
+from numpy import sign
+
+# margins of error when trying the reverse_to_middle approach
+CTR_ANG_MARGIN = 10
+PRP_ANG_MARGIN = 10
+
+# careful acceleration values during crisis handling
+ACC              = 0.7
+REV_ACC          = 0.5
+OFF_ROAD_ACC     = 0.2
+OFF_ROAD_REV_ACC = 0.3
+
+# as stated by Torcs documentation
 MAX_WHEEL_ROTATION = 21
+
+# timers (100 = 2 sec)
+BAD_COUNTER_THRESHOLD = 100
+BAD_COUNTER_MANUAL_THRESHOLD = 600
+APPROACH_TIMEOUT = 1000
 
 # NOTE
 # steer left = positive
@@ -23,28 +37,24 @@ MAX_WHEEL_ROTATION = 21
 
 class CrisisDriver(Driver):
 
-    def __init__(self):
+    def __init__(self, logdata=True):
         """ Handles difficult situations in which the car gets stuck
         """
-        super(CrisisDriver, self).__init__()
+        super(CrisisDriver, self).__init__(logdata=logdata)
+        self.iter = 0
+        self.driver = Driver(logdata=False)
         self.is_in_control = False
-    
-    def pass_control(self, carstate):
-        """ Initializes a new crisis that has not been handled yet
-
-        Args:
-            carstate:   The original carstate
-        
-        """
-        self.is_in_control = True
-        self.approach = 0
-        # check object in front
-        # check car in front
-        # check track angle and side of the road
-        # determine reverse or straight ahead
+        self.approaches = [self.navigate_to_middle, self.original_implementation]
+        self.bad_counter = 0
+        self.needs_help = False
 
     def drive(self, carstate):
-        """ Description
+        """ Gets the car out of a difficult situation
+
+        Tries different approaches and gives each one a time out.
+        A class level variable keeps track of the current approach
+        across game cycles. If the timer runs out, the approach
+        is terminated and the next approach gets initiated.
 
         Args:
             carstate:   All parameters packed in a State object
@@ -53,114 +63,171 @@ class CrisisDriver(Driver):
             command:    The next move packed in a Command object
         
         """
-        # try different approaches and give each one a time out
-        # a class level state will indicate the current approach
-        # if the timer runs out, this approach will be changed
-        # approach 1) reverse towards the road, then once on the road,
-        #             reverse towards the wrong direction untill facing
-        #             the right angle (within a certain margin)
-        return command
+        command = Command()
+        if self.appr_counter < APPROACH_TIMEOUT:
+            self.next_approach()
+        return self.approaches[self.approach](carstate, command)
     
-    def reverse_to_middle(self, carstate, command):
-        """ Reverses the car and finds it way to the middle of the road
+    def pass_control(self, carstate):
+        """ Initializes a new crisis that has not been handled yet
+
+        Args:
+            carstate:   The original carstate
+        
+        """
+        err(self.iter,"CRISIS: control received")
+        self.is_in_control = True
+        self.approach = 0
+        self.appr_counter = 0
+        self.previous_angles = []
+        # check if car in front
+        # check if car behind
+        # check track angle and side of the road
+        # determine reverse or straight ahead
+    
+    def return_control(self):
+        """ Passes control back to the main driver """
+        err(self.iter,"CRISIS: control returned")
+        self.is_in_control = False
+    
+    def next_approach(self):
+        """ Adjusts state to next approach """
+        err(self.iter,"CRISIS: next approach:",
+            self.approaches[self.approach].__name__)
+        self.approach += 1
+        self.appr_counter = 0
+        if self.approach >= len(self.approaches):
+            self.return_control()
+    
+    def approach_succesful(self):
+        """ Called when a technique finished executing
+        """
+        err(self.iter,"CRISIS: approach succesful:",
+            self.approaches[self.approach].__name__)
+        if self.has_problem:
+            self.next_approach()
+        else:
+            self.return_control()
+    
+    def update_status(self, carstate):
+        """ Updates the status of the car regarding its problems
+
+        Args:
+            carstate:   The full carstate
+        
+        """
+        self.iter += 1
+        self.previous_angles.append(carstate.angle)
+
+        self.is_on_left_side  = sign(carstate.distance_from_center) == -1
+        self.is_on_right_side = not self.is_on_left_side
+        self.faces_left   = sign(carstate.angle) == -1
+        self.faces_right  = not self.faces_left
+        self.faces_front  = abs(carstate.angle) < 90
+        self.faces_back   = not self.faces_front
+        self.faces_middle = self.is_on_left_side == self.faces_right
+
+        self.is_standing_still = carstate.speed_x == 0
+        self.has_car_in_front  = car_in_front(carstate.opponents)
+        self.has_car_behind    = car_behind(carstate.opponents)
+        self.is_blocked        = self.is_standing_still and \
+                          (self.has_car_in_front or self.has_car_behind) # TODO when accelerating but speed < 1?
+        self.is_off_road = sign(max(carstate.distances_from_edge)) == -1 # TODO verify
+        self.is_reversed = self.faces_back
+        self.is_going_in_circles = going_in_circles(self.previous_angles)
+        self.has_problem = self.is_off_road or \
+                           self.is_going_in_circles or \
+                           self.is_blocked
+        if self.has_problem:
+            self.bad_counter += 1
+            if self.bad_counter >= BAD_COUNTER_THRESHOLD:
+                self.needs_help = True
+        else:
+            self.bad_counter = 0
+            self.needs_help = False
+
+    #
+    # Approach Implementations
+    #
+    def original_implementation(self, carstate, command):
+        """
+
+        approach 0)
 
         Args:
             carstate:       The full carstate as passed down by the server
+            command:        The command to adjust
         
         """
-        command.gear = -1
-        command.acceleration = REV_ACC
+        command = self.driver.drive(carstate)
+        is_stuck = abs(carstate.speed_x) <= 5 and carstate.current_lap_time >= 10
+        if self.bad_counter >= BAD_COUNTER_MANUAL_THRESHOLD and is_stuck:
+            # we try reversing
+            command.gear = -command.gear
+            if command.gear < 0:
+                command.steering = -command.steering
+                command.gear = -1
+            self.bad_counter = 200
+        return command
+    
+    def navigate_to_middle(self, carstate, command):
+        """ Finds it way to the middle of the road by driving in reverse
+        
+        approach 1) reverse towards the road, then once on the road,
+                    reverse towards the the middle until facing foward
+                    with an angle that's within the margin
 
-        #
-        if is_offroad(carstate):
-            angle_to_road = # TODO angle that lines back of the car facing the road
-            if abs(angle_to_road) < REV_ANG_MARGIN:
-                command.steering = 0
+        Args:
+            carstate:       The full carstate as passed down by the server
+            command:        The command to adjust
+        
+        """
+        if self.is_off_road:
+            perp_angle = 90 * -sign(carstate.distance_from_center)
+            if self.faces_middle:
+                diff_with_perp_angle = perp_angle - carstate.angle
+                if not abs(diff_with_perp_angle) < PRP_ANG_MARGIN:
+                    command.steering = -sign(diff_with_perp_angle)
+                gear = 1
+                acceleration = OFF_ROAD_ACC
             else:
-                command.steering = # TODO steer so that back of the car faces the road
-                command.acceleration = CAREFUL_REV_ACC
-        # if on the road
+                diff_with_perp_angle = perp_angle + carstate.angle
+                if not abs(diff_with_perp_angle) < PRP_ANG_MARGIN:
+                    command.steering = -sign(diff_with_perp_angle)
+                gear = -1
+                acceleration = OFF_ROAD_REV_ACC
         else:
-            # if in the middle of the road
-            if abs(carstate.distance_from_center) < REV_CTR_MARGIN:
-                if abs(carstate.angle) = < REV_ANG_MARGIN:
-                    self.is_in_control = False
-                    command.steering = 0
-                else:
-                    park(carstate, command)
-            # if not in the middle of the road
+            if abs(carstate.angle) < CTR_ANG_MARGIN:
+                self.approach_succesful()
             else:
-                park(carstate, command)
-            
-        command.gear = -1
-        command.acceleration = 0.25
-
+                command.steering = -sign(carstate.distance_from_center)
+                if self.faces_middle:
+                    command.gear = 1
+                    command.acceleration = ACC
+                else:
+                    command.gear = -1
+                    command.acceleration = REV_ACC
         return command
 
-def park(carstate, command):
-    """ Parks the car backwards on the middle of the road
+#
+# Problem Detectors
+#
+def car_in_front(opp):
+    """ Returns True if there's a car in front of ours """
+    return any([o < 1 for o in opp[17:18]])        # TODO are these the 20deg slices in front?
 
-    Args:
-        carstate:   The full car state
-        command:    The command to return
+def car_behind(opp):
+    """ Returns True if there's a car behind ours """
+    return any([0 < 0 for o in opp[0] + opp[35]])  # TODO and these in the back?
 
-    """
-    if abs(carstate.angle) < MAX_WHEEL_ROTATION:
-        command.steering = carstate.angle     # reverse aligns wheels with track
-    else:
-        angle_polarity = carstate.angle / abs(carstate.angle)
-        command.steering = angle_polarity     # turns as much as possible
-
-def car_is_stuck():
-    """ Detects that the car is stuck and requires help
-    
-    Returns:
-        A boolean indicating whether the car is stuck
-    
-    """
-    return
-
-def in_a_bad_place(carstate, bad_counter):
-    """ Description
-
-    Args:
-        carstate:       The full carstate object
-        bad_counter:    ???
-
-    Returns:
-        A boolean indicating whether the car is having troubles
-
-    """
-    something_wrong = False
-    if is_offroad(carstate):
-        #print("I'm offroad!")
-        something_wrong = True
-    if is_reversed(carstate):
-        #print("I'm reversed!")
-        something_wrong = True
-    if is_blocked(carstate):
-        #print("I'm blocked!")
-        something_wrong = True
-    if (something_wrong):
-        bad_counter += 1
-    else:
-        bad_counter = 0
-    # if we have been in a bad place for 2 seconds
-    if bad_counter >= 100:
-        return True
+def going_in_circles(angles):
+    """ Returns True if the car is rapidly going round in circles """
+    # TODO checks if there is a pattern in the angles that
+    # constitutes rotation. Take care of skip from 180 to -180.
     return False
 
-def is_offroad(carstate):
-    """ Returns True if car is off road """
-    return max(carstate.distances_from_edge) == -1
 
-def is_blocked(carstate):
-    """ Returns True if car is stuck """
-    return abs(carstate.speed_x) <= 5 and carstate.current_lap_time >= 10
 
-def is_reversed(carstate):
-    """ Returns True if the car is facing the wrong side """
-    return abs(carstate.angle) >= 90
 
 def err(*args):
     """ prints to standard error """
