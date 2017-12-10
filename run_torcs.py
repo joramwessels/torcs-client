c286710d66c356d9955da32dcd26ac1bbda52bb9
HaukurPall <haukurpalljonsson@gmail.com>
2017-12-10 12:31:24 +0100

    fixing merge issues...

--- run_torcs.py
+++ run_torcs.py
@@ -4,6 +4,8 @@ import multiprocessing
 import subprocess
 from pytocl.main import main
 from combined_driver import Final_Driver
+import os
+import signal
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--drivers', nargs='+', type=str)
@@ -34,12 +36,12 @@ all_tracks = ["aalborg", "alpine-1", "alpine-2", "brondehach", "corkscrew", "e-t
 # elif drivers[0] == "our":
 #     drivers = our_driver
 
-def set_drivers(root, drivers):
+def set_drivers(root, drivers, port_offset):
     xml_drivers = root[4] #Drivers section
     del xml_drivers[3] #a specific driver definition
     for index, driver in enumerate(drivers):
         ele = ET.Element("section", attrib={"name": str(index + 1)})
-        ele.append(ET.Element("attnum", attrib={"name": "idx", "val": str(index )}))
+        ele.append(ET.Element("attnum", attrib={"name": "idx", "val": str(port_offset)}))
         ele.append(ET.Element("attstr", attrib={"name": "module", "val": driver}))
 
         xml_drivers.append(ele)
@@ -48,7 +50,7 @@ def set_track(root, track):
     xml_track = root[1]
     xml_track[1][0].attrib["val"] = track
 
-def write_and_run(tree, track, steering_values, max_speed, timeout):
+def write_and_run(tree, track, steering_values, max_speed, timeout, port):
     file_out = "tmp.quickrace.xml"
     # Write back to file
     tree.write(file_out)
@@ -58,28 +60,56 @@ def write_and_run(tree, track, steering_values, max_speed, timeout):
     # fitness = -running time*100 + distance done
     # print out result fitness
     steering_values = ", ".join([str(x) for x in steering_values])
-    command = "./run_evaluation.sh '{}' {} {}".format(steering_values, max_speed, timeout)
-    print(command)
+    command = "./run_evaluation.sh '{}' {} {} {}".format(steering_values, max_speed, timeout, port)
     #["./run_evaluation.sh", steering_values, str(max_speed), str(timeout)]
     completed_command = subprocess.run(command, shell=True, check=False, stdout=subprocess.PIPE)
+    pids = list(completed_command.stdout.decode("utf-8").strip().split())
+    try:
+        os.kill(pid[0], signal.SIGINT)
+    except:
+        pass
 
+    try:
+        os.kill(pid[1], signal.SIGINT)
+    except:
+        pass
     return completed_command
 
+def run_on_ea_tracks(driver, steering_values, max_speed, timeout):
+    return run_on_tracks(driver, ["ole-road-1", "alpine-1", "forza"], steering_values, max_speed, timeout)
+
 def run_on_all_tracks(driver, steering_values, max_speed, timeout):
+    return run_on_tracks(driver, all_tracks, steering_values, max_speed, timeout)
+
+def run_on_tracks(driver, tracks, steering_values, max_speed, timeout):
     # Open original file
     tree = ET.parse(file_in)
     root = tree.getroot()
 
-    set_drivers(root, [driver])
     client = []
     server = []
-    for track in all_tracks:
+    server_temp_file = "server.out"
+    client_temp_file = "client.out"
+    base_port = 3001
+    port_offset = 0
+    for track in tracks:
+        port = base_port + port_offset
+        set_drivers(root, [driver], port_offset)
+        if os.path.isfile(server_temp_file):
+            os.remove(server_temp_file)
+        if os.path.isfile(client_temp_file):
+            os.remove(client_temp_file)
         set_track(root, track)
-        completed_command = write_and_run(tree, track, steering_values, max_speed, timeout)
+        completed_command = write_and_run(tree, track, steering_values, max_speed, timeout, port)
         with open("server.out") as f:
             server.append(f.readlines())
         with open("client.out") as f:
-            client.append(f.readlines())
+            client.append(list(f.readlines()))
+
+        # we cycle through the ports
+        port_offset += 1
+        if base_port + port_offset > 3009:
+            port_offset = 0
 
     return client, server
 
